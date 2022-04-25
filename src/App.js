7aeef6592cca50d1bb7035c08bda606e79d22475
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-04-25 12:40:19 +0200

    MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const API = "https://api.kanye.rest/";
+
+const Solution = () => {
+  console.log(API)
+  const quote = 'hardcoded quote'  
+  
+  return <h1>{quote}</h1>;
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Solution />
     </div>
   );
-}
+};
 
 export default App;
