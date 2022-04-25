5c7f1b684a39b27ac0018720e9a2cab6a537bc1f
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-04-25 13:18:11 +0200

    clear the interval using the cleanup function!

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ const Solution = () => {
             .then((quote) => setQuote(quote));
 
     useEffect(() => {
-        setInterval(getData, 2000); // instead of executing the function once, we can set up an interval that will call it periodically. React will update our page each time new data gets loaded!
+        const timer = setInterval(getData, 2000); // instead of executing the function once, we can set up an interval that will call it periodically. React will update our page each time new data gets loaded!
+        return () => clearInterval(timer) // if the hook returns a function, it will be called when our component is removed from the page. This is usefull for clearing event handler or intervals which could otherwise cause issues
     }, []);
 
     return <h1>{quote}</h1>;
