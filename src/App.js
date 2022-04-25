87d6ecb8b4f184a83017b5f1f0216c6be76e883f
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-04-25 12:46:11 +0200

    use state to enable reactivity!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
+// our source of data
 const API = "https://api.kanye.rest/";
 
 const Solution = () => {
   console.log(API)
-  const quote = 'hardcoded quote'  
+  // the quote will be changing over time, so we need to use state. Otherwise React won't be able to update it with magic :)
+  const [quote, setQuote] = useState('initial state of quote')  
   
   return <h1>{quote}</h1>;
 };
