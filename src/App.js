a4760db990517c49ecc5551237486cbcd609171a
ranmacar <102212010+WBS-MartinV@users.noreply.github.com>
2022-04-25 12:56:48 +0200

    update our state!

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import React, { useState } from "react";
 // our source of data
 const API = "https://api.kanye.rest/";
 
-// describe what needs to happen to actually get the piece of data we need from the API
-const getData = () =>
-    fetch(API) // call the fetch function
-        .then((result) => result.json()) // wait for the result and extract the JSON data
-        .then((json) => json.quote) // optionally, we can narrow of result here to just the quote
-        .then((quote) => console.log(quote)); // and log it to the console
-
-// above we just declared a function that gets the data. We need to call it to execute the steps it describes.
-getData();
-
 const Solution = () => {
     // the quote will be changing over time, so we need to use state. Otherwise React won't be able to update it with magic :)
     const [quote, setQuote] = useState("initial state of quote");
+
+    // instead of logging the data to the console, we want to update our state!
+    // but our setQuote function is defined here, so we need to move our getData function here, or provide access to it via a parameter
+    const getData = () =>
+        fetch(API) // call the fetch function
+            .then((result) => result.json()) // wait for the result and extract the JSON data
+            .then((json) => json.quote) // optionally, we can narrow of result here to just the quote
+            .then((quote) => setQuote(quote)); // and log it to the console
+
+    // but this will cause an infinite update loop :(
+    getData();
 
     return <h1>{quote}</h1>;
 };
